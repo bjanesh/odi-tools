aaf94e22af4a5c35e68eda5317bfe62ac7089513
Bill Janesh <bjanesh@gmail.com>
2017-01-17 17:14:26 -0500

    fix gaia on podi

--- offlinecats.py
+++ offlinecats.py
@@ -30,7 +30,7 @@ def get_sdss_coords_offline(img, ota, inst,output='test.sdss'):
 
     sdss_cat_img = hdulist['CAT.PHOTCALIB']
     cat_img_data = Table.read(sdss_cat_img, format='fits')
-    print cat_img_data.colnames
+    # print cat_img_data.colnames
     # force little-endian byte order to make FITS play nice with pandas
     sdss_cat_img_df = cat_img_data.to_pandas()
     # sdss_cat_img_df = pd.DataFrame.from_dict(cat_img_dict)
