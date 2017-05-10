cdbadd0e0fa2dc113ee29ab29b5b2ecfa27419d2
Bill Janesh <wjanesh@indiana.edu>
2017-05-10 13:42:46 -0400

    increase median filter window to 51x51

--- illcor.py
+++ illcor.py
@@ -8,6 +8,7 @@ from tqdm import tqdm
 import odi_config as odi
 
 def dark_sky_flat(filter):
+    from cv2 import medianBlur
     med_otalist = []
     print 'making dark sky flats for',filter
     for key in tqdm(odi.OTA_dictionary):
@@ -24,24 +25,52 @@ def dark_sky_flat(filter):
         # iraf.immatch.imcombine.setParam('zero','none')
         iraf.immatch.imcombine.setParam('zero','median')
         iraf.immatch.imcombine(logfile='imcombine.log.txt', mode='h')
-        if key == 1:
-            data,header = odi.fits.getdata(odi.skyflatpath+med_out,header=True)
-            mean, median, std = odi.sigma_clipped_stats(data, sigma=3.0)
-            normalization_factor = median
+        
     iraf.set(clobber = 'yes')
     print 'smoothing dark sky flats for',filter
-    for i in tqdm(range(len(med_otalist))):
+    for key in tqdm(odi.OTA_dictionary):
+        image_list = odi.OTA_dictionary[key]+'.'+filter+'.lis'
+        med_out = image_list.replace('.lis','.med.fits')
+        # don't overwrite the original flat
+        med_smooth = image_list.replace('.lis','.med.smooth.fits')
+
+        # smooth the flat with a 50 x 50 median box
         iraf.unlearn(iraf.imutil.imarith,iraf.imfilter.median)
-        iraf.imutil.imarith.setParam('operand1',odi.skyflatpath+med_otalist[i])
+        iraf.imfilter.median.setParam('input',odi.skyflatpath+med_out)
+        iraf.imfilter.median.setParam('output',odi.skyflatpath+med_smooth)
+        iraf.imfilter.median.setParam('xwindow',51)
+        iraf.imfilter.median.setParam('ywindow',51)
+        iraf.imfilter.median.setParam('zloreject',1.0) # ignore 0.0s in the smoothing, they'll cause image artifacts
+        iraf.imfilter.median(verbose='no', mode='h')
+        
+        # determine the normalization factor from the _smoothed_ image
+        if key == 1:
+            data,header = odi.fits.getdata(odi.skyflatpath+med_smooth,header=True)
+            mean, median, std = odi.sigma_clipped_stats(data, sigma=3.0, mask_value=0.0) # be sure to ignore 0.0s in the flat
+            normalization_factor = median
+        
+        # smoothing using numpy, this method is much slower
+        # data, header = odi.fits.getdata(odi.skyflatpath+med_out, header=True)
+        # # data_gaps = data.astype(bool)
+        # data_smoothed = medianBlur(data, ksize=51)
+        # plt.imshow(data_smoothed, interpolation='nearest')
+        # plt.show()
+        
+        # divide the smoothed image by the normalization factor
+        iraf.imutil.imarith.setParam('operand1',odi.skyflatpath+med_smooth)
         iraf.imutil.imarith.setParam('op','/')
         iraf.imutil.imarith.setParam('operand2',normalization_factor)
-        iraf.imutil.imarith.setParam('result',odi.skyflatpath+med_otalist[i])
+        iraf.imutil.imarith.setParam('result',odi.skyflatpath+med_smooth)
         iraf.imutil.imarith(verbose='no', mode='h')
-        iraf.imfilter.median.setParam('input',odi.skyflatpath+med_otalist[i])
-        iraf.imfilter.median.setParam('output',odi.skyflatpath+med_otalist[i])
-        iraf.imfilter.median.setParam('xwindow',50)
-        iraf.imfilter.median.setParam('ywindow',50)
-        iraf.imfilter.median(verbose='no', mode='h')
+        
+        # make an image of 0s and 1s to restore the cell gaps
+        # iraf.imutil.imexpr('(a == 0) ? 0 : 1', odi.skyflatpath+'temp_gaps', odi.skyflatpath+med_smooth, verbose='no')
+        # iraf.imutil.imarith.setParam('operand1',odi.skyflatpath+med_smooth)
+        # iraf.imutil.imarith.setParam('op','*')
+        # iraf.imutil.imarith.setParam('operand2',odi.skyflatpath+'temp_gaps')
+        # iraf.imutil.imarith.setParam('result',odi.skyflatpath+med_smooth)
+        # iraf.imutil.imarith(verbose='no', mode='h')
+        iraf.imutil.imdelete('temp_gaps', mode='h')
     iraf.set(clobber = 'no')
 
     return normalization_factor
@@ -60,7 +89,9 @@ def illumination_corrections(image_to_correct, correction_image, corrected_image
     iraf.imutil.imarith(mode='h')
 
 def main():
-	pass
+    odi.OTA_dictionary = odi.podi_dictionary
+    norm = dark_sky_flat('odi_g')
+    print norm
 
 if __name__ == '__main__':
-	main()
+    main()
