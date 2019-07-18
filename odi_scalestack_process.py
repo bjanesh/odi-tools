fix scaling/stacking defaulting to podi footprint

odi_scalestack_process.py
@@ -18,7 +18,7 @@ except IOError:
     exit()
 
 source = 'sdss'
-inst = odi.instrument(images[filters[0]][0])
+inst = odi.instrument(instrument)
 
 imgnum,fwhm_d,zp_med, zp_std, bg_mean, bg_median, bg_std = np.loadtxt('derived_props.txt',usecols=(0,3,4,5,6,7,8),unpack=True)
 ota_d, filt_d = np.loadtxt('derived_props.txt',usecols=(1,2),unpack=True,dtype=str)